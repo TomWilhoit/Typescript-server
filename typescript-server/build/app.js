9de2f94ae0739570f759d82bff7cb67bb0459e89
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-06-16 21:30:25 -0600

    Creates module export for key

--- typescript-server/build/app.js
+++ typescript-server/build/app.js
@@ -2,13 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
+var unirest = require("unirest");
+var https = require("https");
+var key = require("./key")
 
-app.locals = [1,2,3]
+app.locals = [1, 2, 3];
 
-app.get('/', function (req, res) {
-    res.send(app.locals);
+console.log(key)
+
+app.get("/", (req, res) => {
+  unirest
+    .get("https://wordsapiv1.p.rapidapi.com/words/?random=true")
+    .header("X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com")
+    .header(
+      "X-RapidAPI-Key",
+      key
+    )
+    .end(response => {
+      res.status(200).send(response.body);
+    });
 });
 
-app.listen(3001, function () {
-    console.log('Running on port 3001');
+app.listen(3001, function() {
+  console.log(key);
 });
